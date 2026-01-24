46100b99aa67de4c1b53f13b0ed09aaa241e95a5
nasir uddin <nunnasir@gmail.com>
2026-01-24 16:18:29 +0600

    Update Product API

--- SmartInventory.Api/Controllers/ProductController.cs
+++ SmartInventory.Api/Controllers/ProductController.cs
@@ -149,4 +149,80 @@ public class ProductController : ControllerBase
 
         return NoContent();
     }
+
+    /// <summary>
+    /// Get all orders for a user
+    /// </summary>
+    /// <param name="id">User ID</param>
+    /// <returns>List of orders for the user</returns>
+    /// <response code="200">Returns the list of orders</response>
+    /// <response code="404">User not found</response>
+    [HttpGet("users/{id}/orders")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<IEnumerable<object>>> GetUserOrders(int id)
+    {
+        // TODO: Implement order service to get orders by user ID
+        // var result = await _orderService.GetOrdersByUserIdAsync(id);
+        // 
+        // if (!result.Success || result.Data == null)
+        // {
+        //     return NotFound(new { message = result.Error ?? "User not found" });
+        // }
+        //
+        // return Ok(result.Data);
+
+        return Ok(new { message = "Endpoint implemented. Order service needs to be implemented.", userId = id });
+    }
+
+    /// <summary>
+    /// Get a specific order for a user
+    /// </summary>
+    /// <param name="id">User ID</param>
+    /// <param name="orderId">Order ID</param>
+    /// <returns>Order details</returns>
+    /// <response code="200">Returns the order</response>
+    /// <response code="404">Order or user not found</response>
+    [HttpGet("users/{id}/orders/{orderId}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<object>> GetUserOrder(int id, int orderId)
+    {
+        // TODO: Implement order service to get order by user ID and order ID
+        // var result = await _orderService.GetOrderByUserIdAndOrderIdAsync(id, orderId);
+        // 
+        // if (!result.Success || result.Data == null)
+        // {
+        //     return NotFound(new { message = result.Error ?? "Order not found" });
+        // }
+        //
+        // return Ok(result.Data);
+
+        return Ok(new { message = "Endpoint implemented. Order service needs to be implemented.", userId = id, orderId = orderId });
+    }
+
+    /// <summary>
+    /// Get payments for an order
+    /// </summary>
+    /// <param name="orderId">Order ID</param>
+    /// <returns>List of payments for the order</returns>
+    /// <response code="200">Returns the list of payments</response>
+    /// <response code="404">Order not found</response>
+    [HttpGet("orders/{orderId}/payments")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<IEnumerable<object>>> GetOrderPayments(int orderId)
+    {
+        // TODO: Implement payment service to get payments by order ID
+        // var result = await _paymentService.GetPaymentsByOrderIdAsync(orderId);
+        // 
+        // if (!result.Success || result.Data == null)
+        // {
+        //     return NotFound(new { message = result.Error ?? "Order not found" });
+        // }
+        //
+        // return Ok(result.Data);
+
+        return Ok(new { message = "Endpoint implemented. Payment service needs to be implemented.", orderId = orderId });
+    }
 }
